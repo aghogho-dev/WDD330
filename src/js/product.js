5c41942e69eda13dddb9a8fd61aa04ee90d55a75
aghogho <monorienaghogho@gmail.com>
2025-01-20 00:53:03 +0100

    Week02 - Team - Repeat

--- src/js/product.js
+++ src/js/product.js
@@ -8,19 +8,21 @@ const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-console.log(dataSource.findProductById(productId));
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product);
+// }
 
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const productById = await dataSource.findProductById(e.target.dataset.id);
+  product.addToCart(productById);
 }
 
 // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+window.onload = () => {
+  document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
+};
